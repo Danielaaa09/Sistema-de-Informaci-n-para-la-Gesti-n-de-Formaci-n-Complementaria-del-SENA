dc7908e2dc1f2f251cd658207dffa9bee88047a8
tommyXD777 <yessidarias133@gmail.com>
2025-06-24 11:40:42 -0500

    Primer commit - Sistema SENA funcionando

---
+++ frontend/src/utils/mock.js
@@ -0,0 +1,23 @@
+export const fichasMock = [
+  {
+    id: 1,
+    programa: "ADSO",
+    codigo: "123456",
+    fecha: "2025-06-01",
+    estado: "Activa"
+  },
+  {
+    id: 2,
+    programa: "Contabilidad",
+    codigo: "789012",
+    fecha: "2025-06-10",
+    estado: "Cerrada"
+  },
+  {
+    id: 3,
+    programa: "Electricidad",
+    codigo: "456789",
+    fecha: "2025-06-17",
+    estado: "Activa"
+  }
+]
