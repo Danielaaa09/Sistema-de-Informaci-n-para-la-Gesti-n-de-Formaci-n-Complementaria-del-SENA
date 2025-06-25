f086a804e3fb2ec150b27946ae91484b781688cd
Danielaaa09 <daniepardo14@gmail.com>
2025-06-25 14:04:57 -0500

    Login terminado

--- backend/app.js
+++ backend/app.js
@@ -7,11 +7,9 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json()); 
 
-// importar rutas
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes); 
 
-// puerto
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 import './index.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
